6af4f0e09673176c81809d1f4296f2023b362f1e
Alessandro <155005371+3clyp50@users.noreply.github.com>
2024-10-15 23:54:56 +0200

    cleanup and update requirements.txt

--- scripts/gradio_image_mixer.py
+++ scripts/gradio_image_mixer.py
@@ -6,7 +6,14 @@ from einops import rearrange
 from torch import autocast
 from contextlib import nullcontext
 import requests
+import warnings
 import functools
+
+warnings.filterwarnings("ignore", category=DeprecationWarning, module="fastapi") # Filter out specific warning
+
+import sys, os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
 from ldm.models.diffusion.ddim import DDIMSampler
 from ldm.models.diffusion.plms import PLMSSampler
@@ -16,16 +23,16 @@ import clip
 from PIL import Image
 
 from huggingface_hub import hf_hub_download
-ckpt = hf_hub_download(repo_id="lambdalabs/image-mixer", filename="image-mixer-pruned.ckpt")
+ckpt = hf_hub_download(repo_id="lambdalabs/image-mixer", filename="image-mixer-full.ckpt")
 config = hf_hub_download(repo_id="lambdalabs/image-mixer", filename="image-mixer-config.yaml")
 
 device = "cuda:0"
 model = load_model_from_config(config, ckpt, device=device, verbose=False)
-model = model.to(device).half()
+model = model.to(device).float()
 
 clip_model, preprocess = clip.load("ViT-L/14", device=device)
 
-n_inputs = 5
+n_inputs = 2
 
 torch.cuda.empty_cache()
 
@@ -107,7 +114,7 @@ def run(*args):
     conds = torch.cat(conds, dim=0).unsqueeze(0)
     conds = conds.tile(n_samples, 1, 1)
 
-    ims = sample(sampler, model, conds, 0*conds, scale, start_code, ddim_steps=steps)
+    ims = sample(sampler, model, conds, 0*conds, scale, start_code, h=h, w=w, ddim_steps=steps)
     # return make_row(ims)
     return ims
 
@@ -132,18 +139,10 @@ def change_visible(txt1, im1, val):
 
 with gr.Blocks(title="Image Mixer", css=".gr-box {border-color: #8136e2}") as demo:
 
-    gr.Markdown("")
-    gr.Markdown(
-"""
-# Image Mixer
-
-_Created by [Justin Pinkney](https://www.justinpinkney.com) at [Lambda Labs](https://lambdalabs.com/)_
-
-### __Provide one or more images to be mixed together by a fine-tuned Stable Diffusion model.__
-
-![banner-large.jpeg](https://s3.amazonaws.com/moonup/production/uploads/1674039767068-62bd5f951e22ec84279820e8.jpeg)
-
-""")
+    gr.Markdown("""
+    # Image Mixer
+    _Created by [Justin Pinkney](https://www.justinpinkney.com) at [Lambda Labs](https://lambdalabs.com/). Revamped by @3clyp50_
+    """)
 
     btns = []
     txts = []
@@ -151,72 +150,69 @@ _Created by [Justin Pinkney](https://www.justinpinkney.com) at [Lambda Labs](htt
     strengths = []
 
     with gr.Row():
-        for i in range(n_inputs):
-            with gr.Box():
-                with gr.Column():
-                    btn1 = gr.Radio(
-                        choices=["Image", "Text/URL", "Nothing"],
-                        label=f"Input {i} type",
-                        interactive=True,
-                        value="Nothing",
+        with gr.Column():  # Input column
+            for i in range(n_inputs):
+                with gr.Box():
+                    with gr.Column():
+                        btn1 = gr.Radio(
+                            choices=["Image", "Text/URL", "Nothing"],
+                            label=f"Input {i} type",
+                            interactive=True,
+                            value="Nothing",
                         )
-                    txt1 = gr.Textbox(label="Text or Image URL", visible=False, interactive=True)
-                    im1 = gr.Image(label="Image", interactive=True, visible=False, type="pil")
-                    strength = gr.Slider(label="Strength", minimum=0, maximum=5, step=0.05, value=1, interactive=True)
-    
-                    fn = partial(change_visible, txt1, im1)
-                    btn1.change(fn=fn, inputs=[btn1], outputs=[txt1, im1])
-    
-                    btns.append(btn1)
-                    txts.append(txt1)
-                    ims.append(im1)
-                    strengths.append(strength)
-    with gr.Row():
-        cfg_scale = gr.Slider(label="CFG scale", value=3, minimum=1, maximum=10, step=0.5)
-        n_samples = gr.Slider(label="Num samples", value=2, minimum=1, maximum=4, step=1)
-        seed = gr.Slider(label="Seed", value=0, minimum=0, maximum=10000, step=1)
-        steps = gr.Slider(label="Steps", value=30, minimum=10, maximum=100, step=5)
+                        txt1 = gr.Textbox(
+                            label="Text or Image URL", visible=False, interactive=True
+                        )
+                        im1 = gr.Image(
+                            label="Image",
+                            interactive=True,
+                            visible=False,
+                            type="pil",
+                        )
+                        strength = gr.Slider(
+                            label="Strength",
+                            minimum=0,
+                            maximum=4,
+                            step=0.05,
+                            value=1,
+                            interactive=True,
+                        )
 
-    with gr.Row():
-        submit = gr.Button("Generate")
-    output = gr.Gallery().style(grid=[1,2], height="640px")
+                        fn = partial(change_visible, txt1, im1)
+                        btn1.change(fn=fn, inputs=[btn1], outputs=[txt1, im1])
+
+                        btns.append(btn1)
+                        txts.append(txt1)
+                        ims.append(im1)
+                        strengths.append(strength)
+
+        with gr.Column():  # Settings and Gallery column
+            with gr.Box():  # Settings Box
+                with gr.Row():
+                    cfg_scale = gr.Slider(
+                        label="CFG scale",
+                        value=3.5,
+                        minimum=1,
+                        maximum=20,
+                        step=0.5,
+                    )
+                    n_samples = gr.Slider(
+                        label="Num samples", value=1, minimum=1, maximum=2, step=1
+                    )
+                with gr.Row():
+                    seed = gr.Slider(
+                        label="Seed", value=0, minimum=0, maximum=5000, step=1
+                    )
+                    steps = gr.Slider(
+                        label="Steps", value=40, minimum=10, maximum=100, step=5
+                    )
+            with gr.Row():  # Submit button row
+                submit = gr.Button("Genera immagine")
+
+            output = gr.Gallery().style(grid=[1,2], height="640px")  # Gallery
 
     inps = list(chain(btns, txts, ims, strengths))
     inps.extend([cfg_scale,n_samples,seed, steps,])
     submit.click(fn=run, inputs=inps, outputs=[output])
 
-    ex = gr.Examples([[
-        "Image", "Image", "Text/URL", "Nothing", "Nothing",
-        None,None,"wild flowers blooming",None,None,
-        "assets/im-examples/vermeer.jpg","assets/im-examples/matisse.jpg",None,None,None,
-        1,1,1.8,1,1,
-        3.0, 2, 0, 30,
-    ]],
-    inputs=inps, outputs=[output])
-
-    gr.Markdown(
-"""
-
-## Tips
-
-- You can provide between 1 and 5 inputs, these can either be an uploaded image a text prompt or a url to an image file.
-- The order of the inputs shouldn't matter, any images will be centre cropped before use.
-- Each input has an individual strength parameter which controls how big an influence it has on the output.
-- Using only text prompts doesn't work well, make sure there is at least one image or URL to an image.
-- The parameters on the bottom row such as cfg scale do the same as for a normal Stable Diffusion model.
-- Balancing the different inputs requires tweaking of the strengths, I suggest getting the right balance for a small number of samples and with few steps until you're
-happy with the result then increase the steps for better quality.
-- Outputs are 640x640 by default.
-
-## How does this work?
-
-This model is based on the [Stable Diffusion Image Variations model](https://huggingface.co/lambdalabs/sd-image-variations-diffusers)
-but it has been fined tuned to take multiple CLIP image embeddings. During training, up to 5 random crops were taken from the training images and
-the CLIP image embeddings were computed, these were then concatenated and used as the conditioning for the model. At inference time we can combine the image
-embeddings from multiple images to mix their concepts (and we can also use the text encoder to add text concepts too).
-
-The model was trained on a subset of LAION Improved Aesthetics at a resolution of 640x640 and was trained using 8xA100 GPUs on [Lambda GPU Cloud](https://lambdalabs.com/service/gpu-cloud).
-
-""")
-
-demo.launch(share=True)
+demo.launch(server_name="0.0.0.0", server_port=7860, share=True)
